449896f07691966d6ed469e74852d790b529d2c6
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-07-05 20:11:06 +0000

    use npm_install job

--- src/gitlab/pipeline.ts
+++ src/gitlab/pipeline.ts
@@ -1,11 +1,10 @@
 import { GitlabCI } from "https://deno.land/x/fluent_gitlab_ci@v0.3.2/mod.ts";
-import { npmBuild, npmInstall, npmTest, yarnInstall } from "./jobs.ts";
+import { npmBuild, npmInstall, npmTest } from "./jobs.ts";
 
 const gitlabci = new GitlabCI()
   .image("node:latest")
   .cache(["node_modules/"])
   .addJob("npm_install", npmInstall)
-  .addJob("yarn_install", yarnInstall)
   .addJob("npm_build", npmBuild)
   .addJob("npm_test", npmTest);
 
