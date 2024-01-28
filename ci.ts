eadc0c3cc0bca47bf2abe633da5041cbce9a0277
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-01-28 10:45:27 +0000

    bump version code

--- ci.ts
+++ ci.ts
@@ -1,7 +1,7 @@
 import {
   test,
   build,
-} from "https://pkg.fluentci.io/nodejs_pipeline@v0.6.1/mod.ts";
+} from "https://pkg.fluentci.io/nodejs_pipeline@v0.6.3/mod.ts";
 
 await test();
 await build();
